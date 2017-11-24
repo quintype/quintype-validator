7034f07c5c295ead38499ce8fabe32b9f1e6f48f
Tejas Dinkar <tejas@gja.in>
2017-11-25 00:11:37 +0530

    Adding some better CSS

--- app/client/app.js
+++ app/client/app.js
@@ -22,28 +22,45 @@ class GetUrlComponent extends React.Component {
 }
 
 class ResultSection extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      active: false
+    }
+  }
+
   render() {
     const errors = this.props.result.errors || [];
     const warnings = this.props.result.warnings || [];
-    return <section className={`result result-${this.props.result.status}`}>
-      <h2>{this.props.title}</h2> - <h3>{this.props.result.status}</h3>
-      {this.props.children}
-      {errors.length > 0 && <div>
-        <h4>Errors:</h4>
-        <ul className="result-errors">{errors.map((error, index) => <li key={index}>{error}</li>)}</ul>
-      </div>}
+    const expandable = this.props.children || errors.length > 0 || warnings.length > 0;
+    return <section className={`result ${this.state.active && 'active'}`}>
+      <header className={`result-header ${expandable && "expandable"}`} onClick={() => expandable && this.setState({active: !this.state.active})}>
+        <div className="clearfix">
+          <h2 className="result-title">{this.props.title}</h2>
+          <h3 className={`result-status result-status-${this.props.result.status} ${warnings.length > 0 ? "result-status-WARN" : ""}`}>
+            {this.props.result.status}
+          </h3>
+        </div>
+      </header>
+      <div className="result-body">
+        {this.props.children}
+        {errors.length > 0 && <div>
+          <h4>Errors:</h4>
+          <ul className="result-errors">{errors.map((error, index) => <li key={index}>{error}</li>)}</ul>
+        </div>}
 
-      {warnings.length > 0 && <div>
-        <h4>Warnings:</h4>
-        <ul className="result-warnings">{warnings.map((error, index) => <li key={index}>{error}</li>)}</ul>
-      </div>}
+        {warnings.length > 0 && <div>
+          <h4>Warnings:</h4>
+          <ul className="result-warnings">{warnings.map((error, index) => <li key={index}>{error}</li>)}</ul>
+        </div>}
+      </div>
     </section>
   }
 }
 
 class Results extends React.Component {
   render() {
-    return <div>
+    return <div className="results">
       <ResultSection title="AMP" result={this.props.results.amp} />
       <ResultSection title="Headers" result={this.props.results.headers} />
       <ResultSection title="Structured Data" result={this.props.results.structured} />

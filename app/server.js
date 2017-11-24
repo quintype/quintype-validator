b13e1cfa515ff4ae6c93b6899b0bded1d4ddb3f8
Tejas Dinkar <tejas@gja.in>
2017-11-24 18:09:35 +0530

    Testing for h1

--- app/server.js
+++ app/server.js
@@ -44,25 +44,77 @@ function runAmpValidator(url, dom) {
       return {
         status: result.status,
         ampUrl: ampUrl,
-        errors: result.errors
-                      .filter(({severity}) => severity == "ERROR")
-                      .map(ampErrorToMessage),
-        warnings: result.errors
-                        .filter(({severity}) => severity != "ERROR")
-                        .map(ampErrorToMessage)
+        errors: result.errors.filter(({severity}) => severity == "ERROR").map(ampErrorToMessage),
+        warnings: result.errors.filter(({severity}) => severity != "ERROR").map(ampErrorToMessage)
       }
     });
+}
+
+function validate($, selector, contentAttr, errors, rules = {}) {
+  const elements = $(selector);
+
+  if(rules.presence && elements.length == 0)
+    return errors.push(`Could not find an element with selector ${selector}`);
+  else if (rules.count && elements.length != rules.count)
+    return errors.push(`Expected to find ${rules.count} elements with selector ${selector}, got ${elements.length}`)
+
+  elements.each((i, element) => {
+    const content = contentAttr == 'body' ? $(element).html() : $(element).attr(contentAttr);
+    if(rules.presence && (!content || content == ''))
+      return errors.push(`Found an empty ${selector}`)
+
+    if(rules.length_le && content.length > rules.length_le)
+      return errors.push(`Content in ${selector} is longer than ${rules.length_le}`)
+  });
+}
+
+function runSeoValidator(url, dom) {
+  var errors = [];
+  var warnings = [];
+
+  validate(dom, "head title", 'body', errors, {count: 1, presence: true});
+
+  validate(dom, "body h1", 'body', errors, {count: 1, presence: true});
+  validate(dom, "body h1", 'body', warnings, {length_le: 66});
+
+  validate(dom, "meta[name=description]", 'content', errors, {count: 1, presence: true})
+  validate(dom, "meta[name=description]", 'content', warnings, {length_le: 160})
+
+  return Promise.resolve({
+    status: errors.length == 0 ? "PASS" : "FAIL",
+    errors: errors,
+    warnings: warnings
+  });
+}
+
+function runOgTagValidator(url, dom) {
+  var errors = [];
+  var warnings = [];
+
+  validate(dom, "meta[property=og\\:type]", 'content', errors, {count: 1, presence: true, length_le: 12});
+  validate(dom, "meta[property=og\\:title]", 'content', warnings, {count: 1, presence: true, length_le: 66});
+  validate(dom, "meta[property=og\\:site_name]", 'content', errors, {count: 1, presence: true, length_le: 30});
+
+  validate(dom, "meta[property=og\\:image]", 'content', errors, {count: 1, presence: true});
+  validate(dom, "meta[property=og\\:image\\:height]", 'content', warnings, {count: 1, presence: true});
+  validate(dom, "meta[property=og\\:image\\:width]", 'content', warnings, {count: 1, presence: true});
+
+  return Promise.resolve({
+    status: errors.length == 0 ? "PASS" : "FAIL",
+    errors: errors,
+    warnings: warnings
+  });
 }
 
 app.post("/validate.json", (req, res) => {
   const url = req.body.url;
   rp(url)
     .then(htmlString => cheerio.load(htmlString))
-    .then(dom => runAmpValidator(url, dom))
-    .then((result) => {
+    .then(dom => Promise.all([runAmpValidator(url, dom), runSeoValidator(url, dom), runOgTagValidator(url, dom)]))
+    .then(([amp, seo, og]) => {
       res.status(201);
       res.setHeader("Content-Type", "application/json");
-      res.json(result);
+      res.json({seo, amp, og});
     })
     .catch(error => {
       res.status(500);

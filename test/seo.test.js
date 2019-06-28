67c88a874c41c87287bfd9c031926bc6fc7dbe19
Abhinaya Sukumar <abhinaya.sukumar@quintype.com>
2019-06-28 10:39:32 +0530

    Add assert.js and fix seo test assertion

--- test/seo.test.js
+++ test/seo.test.js
@@ -1,38 +1,54 @@
-const seo = require("../app/story/seo_new.js");
-const assert = require("assert");
+const seo = require("../app/story/seo.js");
+const expect = require("expect");
 
 describe("seo stats", () => {
   const story = {
-    title: "john wick foo",
+    title: "john wick foo content",
     "meta-description": "metadata",
-    content: "this is my content then",
-    images: {
-      image: "<img><img>",
-      "alt-text": "alt text"
-    }
+    content:
+      "this is my content then this is my content then this is my content then this is my content then this is my content then this is my content then this is my content then this is my content then this is my content then this is my content then this is my content then this is my content then this is my content then this is my content then this is my content then",
+    images: [
+      {
+        image: "<img><img>",
+        "alt-text": "alt text"
+      }
+    ]
   };
 
   expectedSeoStats = {
     title: {
-      errors: ["title doesn't contain focus keyword."],
-      warnings: [],
-      goodies: ["You've entered title", "The title length is perfect."]
-    },
-    "meta-description": {
-      errors: ["meta-description doesn't contain focus keyword."],
-      warnings: ["The meta-description is too short."],
-      goodies: ["You've entered meta-description"]
-    },
-    content: {
-      errors: ["content doesn't contain focus keyword."],
-      warnings: ["The content word count is too short."],
-      goodies: ["You've entered content"]
-    },
-    images: {
       errors: [],
       warnings: [],
-      goodies: []
+      goodies: [
+        "You've added title",
+        "The title contains focus keyword 'Content'.",
+        "The title length is perfect."
+      ]
+    },
+    "meta-description": {
+      errors: ["The meta-description doesn't contain focus keyword 'Content'."],
+      warnings: ["The meta-description is too short."],
+      goodies: ["You've added meta-description"]
+    },
+    content: {
+      errors: [],
+      warnings: ["The content word count is too short."],
+      goodies: [
+        "You've added content",
+        "The content contains focus keyword 'Content'.",
+        'Your keyword density 1% is pretty perfect, focus keyword "Content" is used 1 time(s)'
+      ]
+    },
+    images: {
+      errors: [
+        "You have not used focus keyword 'Content' in the alt tag of image"
+      ],
+      warnings: [],
+      goodies: [
+        "You've added images",
+        "You have used Alt attribute in all images"
+      ]
     }
   };
-  assert.deepEqual(seo(story), expectedSeoStats)
+  expect(seo(story, "Content")).toStrictEqual(expectedSeoStats);
 });

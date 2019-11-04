6c0ec0c351982ee773bfd2fbf804b62ca6d50871
robert christopher <antonyrobertchristopher@gmail.com>
2019-11-04 15:20:57 +0530

    Updates

---
+++ precache-manifest.4d6ab4e559deaea24d7c769ffed473fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38a0c4f3f98247a90a7a0bb82cc314f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dff89f6635ad5fa30013",
+    "url": "/static/css/main.464f843f.chunk.css"
+  },
+  {
+    "revision": "82fb3e6742216d42de92",
+    "url": "/static/js/2.eeb99682.chunk.js"
+  },
+  {
+    "revision": "dff89f6635ad5fa30013",
+    "url": "/static/js/main.0c884893.chunk.js"
+  },
+  {
+    "revision": "6f05875d684597604f8e",
+    "url": "/static/js/runtime-main.fdf0e909.js"
+  },
+  {
+    "revision": "fb53b78811f7326c2a6a502d0732094c",
+    "url": "/static/media/infinity.fb53b788.svg"
+  }
+]);

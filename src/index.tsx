0692b198cdd81d85387bc1a08cbee5c0819154ba
robert christopher <antonyrobertchristopher@gmail.com>
2019-11-04 15:15:40 +0530

    moving some files

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {HomeComponent} from './client/home-component';
+import {HomeComponent} from './home-component';
 
 ReactDOM.render(<HomeComponent />, document.getElementById('root'));

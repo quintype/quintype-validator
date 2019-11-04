3394107b988568f1bf0e44f70acad2d57c0bdf75
robert christopher <antonyrobertchristopher@gmail.com>
2019-11-04 15:30:42 +0530

    Updates

---
+++ precache-manifest.691aa89c435a10f7a1f50aa7ef4fff4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c962a102c7bddc046c5565fe3052e508",
+    "url": "/quintype-validator/index.html"
+  },
+  {
+    "revision": "092c54f69312d67a65c6",
+    "url": "/quintype-validator/static/css/main.208de72c.chunk.css"
+  },
+  {
+    "revision": "b4c217a1a63c7bb90fc2",
+    "url": "/quintype-validator/static/js/2.fc83c73b.chunk.js"
+  },
+  {
+    "revision": "092c54f69312d67a65c6",
+    "url": "/quintype-validator/static/js/main.0c884893.chunk.js"
+  },
+  {
+    "revision": "131b2056aa79a3f8ece5",
+    "url": "/quintype-validator/static/js/runtime-main.198ebd91.js"
+  },
+  {
+    "revision": "fb53b78811f7326c2a6a502d0732094c",
+    "url": "/quintype-validator/static/media/infinity.fb53b788.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4d6ab4e559deaea24d7c769ffed473fb.js"
+  "/quintype-validator/precache-manifest.691aa89c435a10f7a1f50aa7ef4fff4c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/quintype-validator/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });

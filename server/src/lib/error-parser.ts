947079c497fdf89170de27c2815b771f32b67399
Priyanka-ashok <priyanka.ashok@quintype.com>
2020-01-07 11:31:41 +0530

    Moved validator from client-migration

---
+++ server/src/lib/error-parser.ts
@@ -0,0 +1,44 @@
+import ajv from 'ajv';
+
+interface ErrorMessage {
+  readonly message: string;
+  readonly logLevel: "warn" | "error";
+  readonly schema?: object;
+}
+
+const getErrorMessageFunction = (keyword: String): (error: ajv.ErrorObject, identifier: String, schema: String) => ErrorMessage => {
+  switch (keyword) {
+    case "additionalProperties":
+      return (error: ajv.ErrorObject, identifier: String, schema: String): ErrorMessage => {
+        const params = error.params as ajv.AdditionalPropertiesParams
+        return {
+          "message": `${schema} with id ${identifier} has additional properties ${params.additionalProperty} ${error.dataPath !== '' ? `in ${error.dataPath}` : ''}`,
+          "logLevel": "warn"
+        };
+      }
+    case "type":
+      return (error: ajv.ErrorObject, identifier: String, schema: String): ErrorMessage => {
+        return { "message": `${schema} with id ${identifier} has wrong type for ${error.dataPath}. It ${error.message}`, "logLevel": "error" }
+      }
+    case "required":
+      return (error: ajv.ErrorObject, identifier: String, schema: String): ErrorMessage => {
+        return { "message": `${schema} with id ${identifier}  ${error.message}`, "logLevel": "error" }
+      }
+    case "anyOf":
+      return (error: ajv.ErrorObject, identifier: String, schema: String): ErrorMessage => {
+        return { "message": `${schema} with id ${identifier} - has  wrong schema in ${error.dataPath}`, "logLevel": "error"}
+      }
+    default:
+      return (error: ajv.ErrorObject, identifier: String, schema: String): ErrorMessage => {
+        console.log(identifier,schema);
+        return { message: error.message || '', "logLevel": "warn" };
+      }
+  }
+}
+
+export function errorParser(errors: ReadonlyArray<ajv.ErrorObject>, identifier: String, schema: String)
+  : ReadonlyArray<ErrorMessage> {
+  return errors.map(error => {
+    return getErrorMessageFunction(error.keyword)(error, identifier, schema)
+  });
+}

db92791b432e58796364d650bb22d440afa8be09
Priyanka-ashok <45617008+Priyanka-ashok@users.noreply.github.com>
2020-01-09 11:13:53 +0530

    Add Sidebar to validator, Add route to navigate to other pages (#17)
    * Add Sidebar to validator, Add route to navigate to other pages
    
    * Add Toggle functionality and CSS Changes
    
    * Removed CSS
    
    * Modified by adding activeClassName
    
    * fix(CSS issue)
    
    * PR Changes

--- client/src/home-component.js
+++ client/src/home-component.js
@@ -1,12 +1,13 @@
-import './app.scss';
+import "./app.scss";
 
-import React from 'react';
-import request from 'superagent-bluebird-promise';
-import Dropzone from 'react-dropzone';
-import _ from 'lodash';
+import React from "react";
+import request from "superagent-bluebird-promise";
+import Dropzone from "react-dropzone";
+import { NavLink } from "react-router-dom";
+import _ from "lodash";
 
-import FileSaver from 'file-saver';
-
+import FileSaver from "file-saver";
+import QuintypeLogo from "./quintype-logo";
 class GetUrlComponent extends React.Component {
   submit(e) {
     e.preventDefault();
@@ -14,27 +15,73 @@ class GetUrlComponent extends React.Component {
   }
 
   importResult(accepted) {
-    if(accepted.length !== 1)
-      return;
+    if (accepted.length !== 1) return;
 
     const reader = new FileReader();
-    reader.onload = (x) => this.props.onImport(JSON.parse(x.target.result));
+    reader.onload = x => this.props.onImport(JSON.parse(x.target.result));
     reader.readAsText(accepted[0]);
   }
 
   render() {
-    return <Dropzone accept="application/json"
-                     onDrop={(accepted) => this.importResult(accepted)}
-                     className="url-dropzone"
-                     acceptClassName="url-dropzone-accept"
-                     rejectClassName="url-dropzone-reject"
-                     disableClick={true}>
-      <form className="url-container" onSubmit={(e) => this.submit(e)} >
-        <input className="url-input" value={this.props.url} placeholder="Enter Url" onChange={(e) => this.props.onChange(e.target.value)}/>
-        <input type="submit" className="url-go" value="Go!" />
-      </form>
-    </Dropzone>;
+    return (
+      <React.Fragment>
+        <Dropzone
+          accept="application/json"
+          onDrop={accepted => this.importResult(accepted)}
+          className="url-dropzone"
+          acceptClassName="url-dropzone-accept"
+          rejectClassName="url-dropzone-reject"
+          disableClick={true}
+        >
+          <form className="url-container" onSubmit={e => this.submit(e)}>
+            <input
+              className="url-input"
+              value={this.props.url}
+              placeholder="Enter Url"
+              onChange={e => this.props.onChange(e.target.value)}
+            />
+            <input type="submit" className="url-go" value="Go!" />
+          </form>
+        </Dropzone>
+      </React.Fragment>
+    );
   }
+}
+
+export class Sidebar extends React.Component {
+  render() {
+    return (
+      <div className="sidebar">
+        <div className="sidebar-container">
+          <QuintypeLogo />
+        </div>
+        <div className="menu">
+          <div className="menu__container">
+          <NavLink to="/website" className="navbar" activeClassName="is-active">
+            <div className="menu__website logo"></div>
+          </NavLink>
+          </div>
+          <div className="menu__container">
+          <NavLink
+            to="/migrator"
+            className="navbar"
+            activeClassName="is-active"
+          >
+            <div className="menu__migrator logo"></div>
+          </NavLink>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export function Migrator() {
+  return (
+    <div>
+      
+    </div>
+  );
 }
 
 class ResultSection extends React.Component {
@@ -42,45 +89,71 @@ class ResultSection extends React.Component {
     super(props);
     this.state = {
       active: false
-    }
+    };
   }
 
   render() {
     const errors = this.props.result.errors || [];
     const warnings = this.props.result.warnings || [];
-    const expandable = this.props.children || errors.length > 0 || warnings.length > 0;
-    return <section className={`result ${this.state.active && 'active'}`}>
-      <header className={`result-header ${expandable && "expandable"}`} onClick={() => expandable && this.setState({active: !this.state.active})}>
-        <div className="clearfix">
-          <h2 className="result-title">{this.props.title}</h2>
-          <h3 className={`result-status result-status-${this.props.result.status} ${warnings.length > 0 ? "result-status-WARN" : ""}`}>
-            {this.props.result.status}
-          </h3>
+    const expandable =
+      this.props.children || errors.length > 0 || warnings.length > 0;
+    return (
+      <section className={`result ${this.state.active && "active"}`}>
+        <header
+          className={`result-header ${expandable && "expandable"}`}
+          onClick={() =>
+            expandable && this.setState({ active: !this.state.active })
+          }
+        >
+          <div className="clearfix">
+            <h2 className="result-title">{this.props.title}</h2>
+            <h3
+              className={`result-status result-status-${
+                this.props.result.status
+              } ${warnings.length > 0 ? "result-status-WARN" : ""}`}
+            >
+              {this.props.result.status}
+            </h3>
+          </div>
+        </header>
+        <div className="result-body">
+          {this.props.children && <div>{this.props.children}</div>}
+
+          {errors.length > 0 && (
+            <div>
+              <h4>Errors:</h4>
+              <ul>
+                {errors.map((error, index) => (
+                  <li key={index}>{error}</li>
+                ))}
+              </ul>
+            </div>
+          )}
+
+          {warnings.length > 0 && (
+            <div>
+              <h4>Warnings:</h4>
+              <ul>
+                {warnings.map((error, index) => (
+                  <li key={index}>{error}</li>
+                ))}
+              </ul>
+            </div>
+          )}
         </div>
-      </header>
-      <div className="result-body">
-        {this.props.children && <div>{this.props.children}</div>}
-
-        {errors.length > 0 && <div>
-          <h4>Errors:</h4>
-          <ul>{errors.map((error, index) => <li key={index}>{error}</li>)}</ul>
-        </div>}
-
-        {warnings.length > 0 && <div>
-          <h4>Warnings:</h4>
-          <ul>{warnings.map((error, index) => <li key={index}>{error}</li>)}</ul>
-        </div>}
-      </div>
-    </section>
+      </section>
+    );
   }
 }
 
 function toRow(category, key, value) {
-  return <tr key={`${category}/${key}`}>
-    <td>{category}</td>
-    <td className="debug-key">{key}</td>
-    <td>{value}</td>
-  </tr>;
+  return (
+    <tr key={`${category}/${key}`}>
+      <td>{category}</td>
+      <td className="debug-key">{key}</td>
+      <td>{value}</td>
+    </tr>
+  );
 }
 
 class DebugSection extends React.Component {
@@ -88,66 +161,122 @@ class DebugSection extends React.Component {
     super(props);
     this.state = {
       active: false
-    }
+    };
   }
 
   render() {
-    return <section className={`result ${this.state.active && 'active'}`}>
-      <header className={`result-header expandable`} onClick={() => this.setState({active: !this.state.active})}>
-        <div className="clearfix">
-          <h2 className="result-title">Debugging</h2>
+    return (
+      <section className={`result ${this.state.active && "active"}`}>
+        <header
+          className={`result-header expandable`}
+          onClick={() => this.setState({ active: !this.state.active })}
+        >
+          <div className="clearfix">
+            <h2 className="result-title">Debugging</h2>
+          </div>
+        </header>
+        <div className="result-body">
+          <div>
+            For a full list of rules, please see{" "}
+            <a
+              href="https://github.com/quintype/quintype-validator"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Readme.md
+            </a>
+          </div>
+          <div>
+            <table>
+              <thead>
+                <tr>
+                  <th>Category</th>
+                  <th>Key</th>
+                  <th>Value</th>
+                </tr>
+              </thead>
+              <tbody>
+                {_(this.props.results)
+                  .entries()
+                  .flatMap(([category, result]) =>
+                    _(result.debug)
+                      .entries()
+                      .map(([key, value]) => toRow(category, key, value))
+                      .value()
+                  )
+                  .value()}
+              </tbody>
+            </table>
+          </div>
+          {this.props.links.length > 0 && (
+            <div>
+              <h4>Links:</h4>
+              <ul>
+                {this.props.links.map((link, index) => (
+                  <li key={index}>
+                    <button onClick={() => this.props.onValidate(link)}>
+                      validate
+                    </button>{" "}
+                    - {link}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
         </div>
-      </header>
-      <div className="result-body">
-        <div>For a full list of rules, please see <a href="https://github.com/quintype/quintype-validator" target="_blank" rel="noopener noreferrer">Readme.md</a></div>
-        <div>
-          <table>
-            <thead>
-              <tr>
-                <th>Category</th>
-                <th>Key</th>
-                <th>Value</th>
-              </tr>
-            </thead>
-            <tbody>
-              {_(this.props.results).entries().flatMap(([category, result]) => _(result.debug).entries().map(([key, value]) => toRow(category, key, value)).value()).value()}
-            </tbody>
-          </table>
-        </div>
-        {this.props.links.length > 0 && <div>
-          <h4>Links:</h4>
-          <ul>{this.props.links.map((link, index) => <li key={index}><button onClick={() => this.props.onValidate(link)}>validate</button> - {link}</li>)}</ul>
-        </div>}
-      </div>
-    </section>
+      </section>
+    );
   }
 }
 
 class Results extends React.Component {
   render() {
-    const showResult = (category, title) => this.props.results[category] ? <ResultSection title={title} result={this.props.results[category]} /> : undefined;
+    const showResult = (category, title) =>
+      this.props.results[category] ? (
+        <ResultSection title={title} result={this.props.results[category]} />
+      ) : (
+        undefined
+      );
 
-    return <div>
-      {showResult("amp", "AMP")}
-      {showResult("headers", "Caching Headers")}
-      {this.props.results.structured
-        ? <ResultSection title="Structured Data" result={this.props.results.structured}>
-            <div>Number of Objects: {this.props.results.structured.numObjects}</div>
+    return (
+      <div className="results">
+        {showResult("amp", "AMP")}
+        {showResult("headers", "Caching Headers")}
+        {this.props.results.structured ? (
+          <ResultSection
+            title="Structured Data"
+            result={this.props.results.structured}
+          >
+            <div>
+              Number of Objects: {this.props.results.structured.numObjects}
+            </div>
             <div>Content Id: {this.props.results.structured.contentId}</div>
           </ResultSection>
-        : undefined}
-      {showResult('og', "Facebook OG Tags")}
-      {showResult('seo', "SEO Rules")}
-      {showResult('robots', "Robots")}
-      {showResult('pagespeed', "PageSpeed")}
-      {showResult('lighthouseSeo', "Lighthouse SEO")}
-      {showResult('lighthousePwa', "PWA")}
-      {showResult('routeData', "Route Data Size")}
+        ) : (
+          undefined
+        )}
+        {showResult("og", "Facebook OG Tags")}
+        {showResult("seo", "SEO Rules")}
+        {showResult("robots", "Robots")}
+        {showResult("pagespeed", "PageSpeed")}
+        {showResult("lighthouseSeo", "Lighthouse SEO")}
+        {showResult("lighthousePwa", "PWA")}
+        {showResult("routeData", "Route Data Size")}
 
-      <DebugSection results={this.props.results} links={this.props.links} onValidate={this.props.onValidate}/>
+        <DebugSection
+          results={this.props.results}
+          links={this.props.links}
+          onValidate={this.props.onValidate}
+        />
 
-      <button className="results-download" onClick={() => this.props.onDownload()}>Download As JSON</button>
-    </div>;
+        <button
+          className="results-download"
+          onClick={() => this.props.onDownload()}
+        >
+          Download As JSON
+        </button>
+      </div>
+    );
   }
 }
 
@@ -163,24 +292,37 @@ export class HomeComponent extends React.Component {
   }
 
   loadRules(url) {
-    request.post(`${process.env.REACT_APP_API_HOST || ""}/api/validate.json`, {url: url})
-           .then(response => this.setState({response: response.body, loading: false, url: response.body.url}))
-           .catch(e => this.setState({loading: false, error: e.message}));
+    request
+      .post(`${process.env.REACT_APP_API_HOST || ""}/api/validate.json`, {
+        url: url
+      })
+      .then(response =>
+        this.setState({
+          response: response.body,
+          loading: false,
+          url: response.body.url
+        })
+      )
+      .catch(e => this.setState({ loading: false, error: e.message }));
   }
 
   processUrl(url) {
-    if(this.state.loading)
-      return;
+    if (this.state.loading) return;
 
-    this.setState({
-      url: url,
-      loading: true,
-      error: null
-    }, () => this.loadRules(url));
+    this.setState(
+      {
+        url: url,
+        loading: true,
+        error: null
+      },
+      () => this.loadRules(url)
+    );
   }
 
   downloadResponse() {
-    const blob = new Blob([JSON.stringify(this.state.response)], {type: "application/json;charset=utf-8"});
+    const blob = new Blob([JSON.stringify(this.state.response)], {
+      type: "application/json;charset=utf-8"
+    });
     FileSaver.saveAs(blob, "validator.json");
   }
 
@@ -190,19 +332,33 @@ export class HomeComponent extends React.Component {
       error: null,
       loading: false,
       url: response.url
-    })
+    });
   }
 
   render() {
-    return <div>
-      <GetUrlComponent onSubmit={(url) => this.processUrl(url)} url={this.state.url} onChange={(url) => this.setState({url: url})} onImport={(result) => this.import(result)}/>
-      {this.state.error && <div className="error-message">{this.state.error}</div>}
-      {this.state.loading && <div className="loading">Crunching Numbers</div>}
-      {!this.state.loading && this.state.response && <Results results={this.state.response.results}
-                                                              links={this.state.response.links}
-                                                              onValidate={(url) => this.processUrl(url)}
-                                                              url={this.state.response.url}
-                                                              onDownload={() => this.downloadResponse()} />}
-    </div>;
+    return (
+      <div>
+        <h1 className="website-heading">Quintype Validator</h1>
+        <GetUrlComponent
+          onSubmit={url => this.processUrl(url)}
+          url={this.state.url}
+          onChange={url => this.setState({ url: url })}
+          onImport={result => this.import(result)}
+        />
+        {this.state.error && (
+          <div className="error-message">{this.state.error}</div>
+        )}
+        {this.state.loading && <div className="loading">Crunching Numbers</div>}
+        {!this.state.loading && this.state.response && (
+          <Results
+            results={this.state.response.results}
+            links={this.state.response.links}
+            onValidate={url => this.processUrl(url)}
+            url={this.state.response.url}
+            onDownload={() => this.downloadResponse()}
+          />
+        )}
+      </div>
+    );
   }
 }
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import {HomeComponent} from './home-component';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import { HomeComponent, Migrator, Sidebar } from './home-component';
 
-ReactDOM.render(<HomeComponent />, document.getElementById('root'));
+class Validator extends React.Component {
+  render() {
+    return (
+      <div>
+        <Router>
+          <Sidebar />
+          <Switch>
+            <Redirect exact from="/" to="/website" />
+            <Route exact path="/website" component={HomeComponent} />
+            <Route exact path="/migrator" component={Migrator} />
+          </Switch>
+        </Router>
+      </div>
+
+    );
+  }
+}
+
+ReactDOM.render(<Validator />, document.getElementById('root'));

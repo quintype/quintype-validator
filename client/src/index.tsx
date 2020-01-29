e1cd3e1af858cd4e97ea424f942f8a41e822633c
Priyanka-ashok <priyanka.ashok@quintype.com>
2020-01-29 09:45:38 +0530

    Refresh issue

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,17 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import { HomeComponent, Migrator, Sidebar } from './home-component';
+import ErrorPage from "./error-404";
 
 class Validator extends React.Component {
   render() {
     return (
       <div>
-        <Router>
+        <Router basename="/quintype-validator">
           <Sidebar />
           <Switch>
             <Redirect exact from="/" to="/website" />
             <Route exact path="/website" component={HomeComponent} />
             <Route exact path="/migrator" component={Migrator} />
+            <Route exact path="" component={ ErrorPage }/>
           </Switch>
         </Router>
       </div>

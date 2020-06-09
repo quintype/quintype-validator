bd3d4ff3ee518225dd7a3200b713d6a346149eff
Radhika <radhika@quintype.com>
2020-06-09 15:30:15 +0530

    add-cypress-test

---
+++ cypress/integration/validateAuthorTest.js
@@ -0,0 +1,13 @@
+import {Validator} from "../pages/validate.page";
+describe("Validate Author", function() {
+    var URL = "?p=/migrator";
+    const validateAuthor = new Validator();
+  
+    it("Should verify validator page with type Author", function() {  
+      cy.visit(URL);
+      validateAuthor.selectAuthorType();
+      validateAuthor.selectValidateByDirectInput();
+      validateAuthor.enterAuthorJsonData();
+      validateAuthor.clickValidateButton();
+    });
+});
---
+++ cypress/utils/readJson.js
@@ -0,0 +1,5 @@
+export function copyJson(jsonFile,selector){
+cy.fixture(jsonFile).then(fixtureContent => {  
+    cy.get(selector).clear().type(JSON.stringify(fixtureContent),{ parseSpecialCharSequences: false });
+  });
+}

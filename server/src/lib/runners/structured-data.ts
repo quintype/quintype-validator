19fff70ed3ac060478c106ee1cd442ecd3f2cf0a
Ramsharan Gorur Jayaraman <sharangj@gmail.com>
2024-11-28 15:16:15 +0530

    Change structured data validator

--- server/src/lib/runners/structured-data.ts
+++ server/src/lib/runners/structured-data.ts
@@ -1,7 +1,16 @@
-import _ from "lodash";
+import _ from 'lodash';
 import rp from 'request-promise';
 
-const structuredErrorToMessage = ({ ownerSet, errorType, args, begin, end }: any) => `[${_.keys(ownerSet).join(" ")}] ${errorType} ${args.join(" ")} (${begin} - ${end})`;
+const structuredErrorToMessage = ({
+  ownerSet,
+  errorType,
+  args,
+  begin,
+  end
+}: any) =>
+  `[${_.keys(ownerSet).join(' ')}] ${errorType} ${args.join(
+    ' '
+  )} (${begin} - ${end})`;
 
 interface StructuredDataAPIResponse {
   readonly errors: ReadonlyArray<{ readonly isSevere: boolean }>;
@@ -10,17 +19,26 @@ interface StructuredDataAPIResponse {
   readonly url: string;
 }
 
-export async function runStructuredDataValidator(_: CheerioSelector, url: string): Promise<ValidationResult> {
+export async function runStructuredDataValidator(
+  _: CheerioSelector,
+  url: string
+): Promise<ValidationResult> {
   try {
-    const body = await rp.post("https://search.google.com/structured-data/testing-tool/validate", {
+    const body = await rp.post('https://validator.schema.org/validate', {
       form: { url }
     });
-    const results = JSON.parse(body.substring(body.indexOf("{"))) as StructuredDataAPIResponse;
+    const results = JSON.parse(
+      body.substring(body.indexOf('{'))
+    ) as StructuredDataAPIResponse;
     const { errors, numObjects, contentId } = results;
-    const actualErrors = errors.filter(error => error.isSevere).map(structuredErrorToMessage);
-    const warnings = errors.filter(error => !error.isSevere).map(structuredErrorToMessage);
+    const actualErrors = errors
+      .filter(error => error.isSevere)
+      .map(structuredErrorToMessage);
+    const warnings = errors
+      .filter(error => !error.isSevere)
+      .map(structuredErrorToMessage);
     if (!contentId) {
-      warnings.push("No ContentId was found");
+      warnings.push('No ContentId was found');
     }
     return {
       errors: actualErrors,
@@ -28,13 +46,13 @@ export async function runStructuredDataValidator(_: CheerioSelector, url: string
       numObjects,
       contentId,
       url: results.url,
-      status: actualErrors.length > 0 ? "FAIL" : numObjects === 0 ? "NA" : "PASS"
+      status:
+        actualErrors.length > 0 ? 'FAIL' : numObjects === 0 ? 'NA' : 'PASS'
     };
-  }
-  catch (e) {
-    return ({
-      status: "ERROR",
+  } catch (e) {
+    return {
+      status: 'ERROR',
       debug: { error: e.message }
-    });
+    };
   }
 }

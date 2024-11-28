3c6134988a50a23d6f70d200b8edeb440313d9c8
Ramsharan Gorur Jayaraman <sharangj@gmail.com>
2024-11-28 16:29:23 +0530

    Keep assets for domain and fix the domain handler

--- server/src/lib/fetch-lighthouse.ts
+++ server/src/lib/fetch-lighthouse.ts
@@ -3,24 +3,31 @@ import rp from 'request-promise';
 export const INVALID_LIGHTHOUSE_SCORE = -1;
 
 interface LighthouseResults {
-  readonly getAudit: (name: string) => { readonly score: number, readonly errors: ReadonlyArray<string> };
-  readonly getDebuggingInfo: (prefix: string) => { readonly [key: string]: number }
+  readonly getAudit: (
+    name: string
+  ) => { readonly score: number; readonly errors: ReadonlyArray<string> };
+  readonly getDebuggingInfo: (
+    prefix: string
+  ) => { readonly [key: string]: number };
 }
 
 export async function fetchLighthouse(url: string): Promise<LighthouseResults> {
-  const psUrl = `https://www.googleapis.com/pagespeedonline/v5/runPagespeed?category=accessibility&category=best-practices&category=performance&category=pwa&category=seo&strategy=mobile&url=${encodeURI(url)}`;
+  const psUrl = `https://www.googleapis.com/pagespeedonline/v5/runPagespeed?category=accessibility&category=best-practices&category=performance&category=seo&strategy=mobile&url=${encodeURI(
+    url
+  )}`;
   try {
     const data = await rp(psUrl, {
       json: true,
       gzip: true
     });
     return new Lighthouse(url, data);
-  } catch(e) {
+  } catch (e) {
     console.error(e.message || e);
     return new LighthouseError(url);
   }
 }
 
+// tslint:disable-next-line:no-class
 class Lighthouse implements LighthouseResults {
   private readonly data: any;
   private readonly url: string;
@@ -30,29 +37,33 @@ class Lighthouse implements LighthouseResults {
     this.data = data;
   }
 
-  public getAudit(name: string): {readonly score: number, readonly errors: ReadonlyArray<string>} {
+  public getAudit(
+    name: string
+  ): { readonly score: number; readonly errors: ReadonlyArray<string> } {
     const category = this.data.lighthouseResult.categories[name];
     return {
       score: category.score * 100,
       errors: category.auditRefs
-        .filter(({weight}: any) => weight > 0)
+        .filter(({ weight }: any) => weight > 0)
         .map(({ id }: any) => this.data.lighthouseResult.audits[id])
         .filter(({ score }: any) => score < 1)
         // tslint:disable-next-line: readonly-keyword
-        .map(({ score, displayValue, title }: any) => `(${score * 100}%) - ${title} (${this.url} ${displayValue || ""})`)
-    }
+        .map(
+          ({ score, displayValue, title }: any) =>
+            `(${score * 100}%) - ${title} (${this.url} ${displayValue || ''})`
+        )
+    };
   }
 
-  public getDebuggingInfo(prefix: string): {readonly [key: string]: number} {
+  public getDebuggingInfo(prefix: string): { readonly [key: string]: number } {
     return {
-      [`${prefix}-fcp`]: this.getAuditScore("first-contentful-paint"),
-      [`${prefix}-fmp`]: this.getAuditScore("first-meaningful-paint"),
-      [`${prefix}-tti`]: this.getAuditScore("interactive"),
-      [`${prefix}-mfid`]: this.getAuditScore("max-potential-fid"),
-      [`${prefix}-si`]: this.getAuditScore("speed-index"),
-      [`${prefix}-ttfb`]: this.getAuditScore("time-to-first-byte"),
-      [`${prefix}-fci`]: this.getAuditScore("first-cpu-idle"),
-    }
+      [`${prefix}-fcp`]: this.getAuditScore('first-contentful-paint'),
+      [`${prefix}-fmp`]: this.getAuditScore('first-meaningful-paint'),
+      [`${prefix}-tti`]: this.getAuditScore('interactive'),
+      [`${prefix}-mfid`]: this.getAuditScore('max-potential-fid'),
+      [`${prefix}-si`]: this.getAuditScore('speed-index'),
+      [`${prefix}-ttfb`]: this.getAuditScore('server-response-time')
+    };
   }
 
   private getAuditScore(name: string): number {
@@ -68,11 +79,13 @@ class LighthouseError implements LighthouseResults {
     this.url = url;
   }
 
-  public getAudit(_: string): { readonly score: number, readonly errors: ReadonlyArray<string> } {
+  public getAudit(
+    _: string
+  ): { readonly score: number; readonly errors: ReadonlyArray<string> } {
     return {
       score: INVALID_LIGHTHOUSE_SCORE,
       errors: [`PageSpeed Crashed While Querying ${this.url}`]
-    }
+    };
   }
 
   public getDebuggingInfo(_: string): { readonly [key: string]: number } {
--- server/src/lib/handlers/validate-domain-handler.ts
+++ server/src/lib/handlers/validate-domain-handler.ts
@@ -1,88 +1,134 @@
 import flatMap from 'array.prototype.flatmap';
-import { Request, Response } from "express";
-import _ from "lodash";
-import rp from "request-promise";
+import { Request, Response } from 'express';
+import rp from 'request-promise';
 
 import { fetchLighthouse, INVALID_LIGHTHOUSE_SCORE } from '../fetch-lighthouse';
-import { fetchUrl, FetchUrlResponse } from "../fetch-url";
+import { fetchUrl, FetchUrlResponse } from '../fetch-url';
 import { runAmpValidator } from '../runners/amp';
-import { runHeaderValidator, runOgTagValidator, runSeoValidator } from "../runners/http";
-import { checkUrls } from "../runners/robots";
+import {
+  runAssetsValidator,
+  runHeaderValidator,
+  runOgTagValidator,
+  runSeoValidator
+} from '../runners/http';
+import { checkUrls } from '../runners/robots';
 import { runRouteDataValidator } from '../runners/route-data';
 
-async function randomStories(baseUrl: string, count: number): Promise<ReadonlyArray<FetchUrlResponse>> {
-  const {stories} = await rp(`${baseUrl}/api/v1/stories?fields=url`, { gzip: true, json: true});
+async function randomStories(
+  baseUrl: string,
+  count: number
+): Promise<ReadonlyArray<FetchUrlResponse>> {
+  const { stories } = await rp(`${baseUrl}/api/v1/stories?fields=url`, {
+    gzip: true,
+    json: true
+  });
   return Promise.all(
     stories
       .sort(() => 0.5 - Math.random())
       .slice(0, count)
-      .map((story: { readonly url: any; }) => fetchUrl(story.url))
+      .map((story: { readonly url: any }) => fetchUrl(story.url))
   );
 }
 
-async function randomSections(baseUrl: string, count: number): Promise<ReadonlyArray<FetchUrlResponse>> {
-  const {sections} = await rp(`${baseUrl}/api/v1/config`, { gzip: true, json: true });
+async function randomSections(
+  baseUrl: string,
+  count: number
+): Promise<ReadonlyArray<FetchUrlResponse>> {
+  const { sections } = await rp(`${baseUrl}/api/v1/config`, {
+    gzip: true,
+    json: true
+  });
   return Promise.all(
     sections
       .sort(() => 0.5 - Math.random())
       .slice(0, count)
-      .map((section: { readonly "section-url": string; }) => fetchUrl(section["section-url"]))
+      .map((section: { readonly 'section-url': string }) =>
+        fetchUrl(section['section-url'])
+      )
   );
 }
 
-async function checkHTTPResponses(pages: ReadonlyArray<FetchUrlResponse>): Promise<{readonly og: ValidationResult, readonly seo: ValidationResult, readonly headers: ValidationResult}> {
+async function checkHTTPResponses(
+  pages: ReadonlyArray<FetchUrlResponse>
+): Promise<{
+  readonly og: ValidationResult;
+  readonly seo: ValidationResult;
+  readonly headers: ValidationResult;
+  readonly assets: ValidationResult;
+}> {
   const responses = await Promise.all(
-    pages.map(async ({url, dom, response}) => ({
+    pages.map(async ({ url, dom, response }) => ({
       url,
       seo: await runSeoValidator(dom, url, response),
       og: await runOgTagValidator(dom, url, response),
-      headers: await runHeaderValidator(dom, url, response)
+      headers: await runHeaderValidator(dom, url, response),
+      assets: await runAssetsValidator(dom, url, response)
     }))
   );
 
   return {
     og: combineResults('og'),
     seo: combineResults('seo'),
-    headers: combineResults('headers')
-  }
+    headers: combineResults('headers'),
+    assets: combineResults('assets')
+  };
 
-  function combineResults(key: 'og' | 'seo' | 'headers'): ValidationResult {
+  function combineResults(
+    key: 'og' | 'seo' | 'headers' | 'assets'
+  ): ValidationResult {
     // tslint:disable-next-line: readonly-array
-    const errors = flatMap(responses, r => (r[key].errors as string[] || []).map(e => `${r.url}: ${e}`));
-    return errors.length
-      ? { status: "FAIL", errors }
-      : { status: "PASS" };
+    const errors = flatMap(responses, r =>
+      ((r[key].errors as string[]) || []).map(e => `${r.url}: ${e}`)
+    );
+    return errors.length ? { status: 'FAIL', errors } : { status: 'PASS' };
   }
 }
 
-async function checkAmp(stories: readonly FetchUrlResponse[]): Promise<ValidationResult> {
-  const ampResults = await Promise.all(stories.map(async ({dom, url}) => ({url, result: await runAmpValidator(dom, url)})));
-  const failingAmpPages = ampResults.filter(({result}) => result.status !== "PASS");
-  const percentage = (failingAmpPages.length * 100 / stories.length);
+async function checkAmp(
+  stories: readonly FetchUrlResponse[]
+): Promise<ValidationResult> {
+  const ampResults = await Promise.all(
+    stories.map(async ({ dom, url }) => ({
+      url,
+      result: await runAmpValidator(dom, url)
+    }))
+  );
+  const failingAmpPages = ampResults.filter(
+    ({ result }) => result.status !== 'PASS'
+  );
+  const percentage = (failingAmpPages.length * 100) / stories.length;
   return {
     status: percentage > 40 ? 'FAIL' : 'PASS',
-    errors: flatMap(failingAmpPages, ({result, url}) => (result.errors || []).map(e => `${url}: ${e}`)),
+    errors: flatMap(failingAmpPages, ({ result, url }) =>
+      (result.errors || []).map(e => `${url}: ${e}`)
+    ),
     debug: [`${100 - percentage}% of tested story pages had AMP versions`]
-  }
+  };
 }
 
-async function checkLighthouseResults(homePage: FetchUrlResponse, story: FetchUrlResponse): Promise<{
-  readonly pagespeed: ValidationResult,
-  readonly lighthouseSeo?: ValidationResult,
-  readonly lighthousePwa?: ValidationResult
+async function checkLighthouseResults(
+  homePage: FetchUrlResponse,
+  story: FetchUrlResponse
+): Promise<{
+  readonly pagespeed: ValidationResult;
+  readonly lighthouseSeo?: ValidationResult;
+  readonly lighthousePwa?: ValidationResult;
 }> {
-  const audits = await Promise.all([fetchLighthouse(homePage.url), fetchLighthouse(story.url)]);
+  const audits = await Promise.all([
+    fetchLighthouse(homePage.url),
+    fetchLighthouse(story.url)
+  ]);
   const [homeAudit, storyAudit] = audits;
 
   const getStatus = (scores: readonly number[], minScore: number) => {
-    if(scores.every(score => score >= minScore)) {
-      return "PASS";
+    if (scores.every(score => score >= minScore)) {
+      return 'PASS';
     } else if (scores.includes(INVALID_LIGHTHOUSE_SCORE)) {
-      return "NA";
+      return 'NA';
     } else {
-      return "FAIL";
+      return 'FAIL';
     }
-  }
+  };
 
   const combineAudits = (category: string, minScore: number) => {
     const results = audits.map(audit => audit.getAudit(category));
@@ -92,32 +138,39 @@ async function checkLighthouseResults(homePage: FetchUrlResponse, story: FetchUr
 
     return {
       status: getStatus(results.map(i => i.score), minScore),
-      errors: [...(errors.length > 0 ? [`Scores - Home ${homeResult.score}%, Story ${storyResult.score}%`] : []), ...errors],
+      errors: [
+        ...(errors.length > 0
+          ? [`Scores - Home ${homeResult.score}%, Story ${storyResult.score}%`]
+          : []),
+        ...errors
+      ],
       debug: {
-        "home-score": homeResult.score > 0 ? homeResult.score : undefined,
-        "story-score": storyResult.score > 0 ? storyResult.score : undefined,
+        'home-score': homeResult.score > 0 ? homeResult.score : undefined,
+        'story-score': storyResult.score > 0 ? storyResult.score : undefined
       }
-    }
-  }
+    };
+  };
 
   const pageSpeedResult = combineAudits('performance', 75);
 
   return {
-    lighthousePwa: combineAudits('pwa', 50),
     lighthouseSeo: combineAudits('seo', 95),
     pagespeed: {
       ...pageSpeedResult,
       debug: {
-        ...homeAudit.getDebuggingInfo("home"),
-        ...storyAudit.getDebuggingInfo("story"),
-        "home-pagespeed": pageSpeedResult.debug["home-score"],
-        "story-pagespeed": pageSpeedResult.debug["story-score"],
+        ...homeAudit.getDebuggingInfo('home'),
+        ...storyAudit.getDebuggingInfo('story'),
+        'home-pagespeed': pageSpeedResult.debug['home-score'],
+        'story-pagespeed': pageSpeedResult.debug['story-score']
       }
-    },
-  }
+    }
+  };
 }
 
-async function checkRouteData(homePage: FetchUrlResponse, story: FetchUrlResponse): Promise<ValidationResult> {
+async function checkRouteData(
+  homePage: FetchUrlResponse,
+  story: FetchUrlResponse
+): Promise<ValidationResult> {
   const [homePageAudit, storyPageAudit] = await Promise.all([
     runRouteDataValidator(homePage.dom, homePage.url),
     runRouteDataValidator(story.dom, story.url)
@@ -125,44 +178,63 @@ async function checkRouteData(homePage: FetchUrlResponse, story: FetchUrlRespons
   return {
     status: homePageAudit.status,
     errors: [
-      ...(homePageAudit.errors || []).map((s: string) => `${s} (${homePage.url})`),
-      ...(storyPageAudit.errors || []).map((s: string) => `${s} (${story.url})`),
+      ...(homePageAudit.errors || []).map(
+        (s: string) => `${s} (${homePage.url})`
+      ),
+      ...(storyPageAudit.errors || []).map((s: string) => `${s} (${story.url})`)
     ],
     debug: {
-      "home-bytes": homePageAudit.debug && (homePageAudit.debug as any).lengthBytes,
-      "story-bytes": storyPageAudit.debug && (storyPageAudit.debug as any).lengthBytes,
+      'home-bytes':
+        homePageAudit.debug && (homePageAudit.debug as any).lengthBytes,
+      'story-bytes':
+        storyPageAudit.debug && (storyPageAudit.debug as any).lengthBytes
     }
-  }
+  };
 }
 
-export async function validateDomainHandler(req: Request, res: Response): Promise<void> {
+export async function validateDomainHandler(
+  req: Request,
+  res: Response
+): Promise<void> {
   try {
     const domain = req.body.url;
     const response = await runAllChecksAgainstDomain(domain);
 
     res
       .status(200)
-      .header("Content-Type", "application/json")
-      .json(response)
+      .header('Content-Type', 'application/json')
+      .json(response);
   } catch (error) {
     // tslint:disable-next-line: no-console
     console.error(error.stack || error);
 
     res
       .status(500)
-      .header("Content-Type", "application/json")
+      .header('Content-Type', 'application/json')
       .json({ error: { message: error.message || error } });
   } finally {
-    res.end()
+    res.end();
   }
 }
 // tslint:disable-next-line: typedef
 export async function runAllChecksAgainstDomain(domain: string) {
   const baseUrl = `https://${domain}`;
-  const [homePage, stories, sections] = await Promise.all([fetchUrl(`${baseUrl}/`), randomStories(baseUrl, 5), randomSections(baseUrl, 2)]);
-  const allPages = [homePage, ...stories, ...sections] as ReadonlyArray<FetchUrlResponse>;
+  const [homePage, stories, sections] = await Promise.all([
+    fetchUrl(`${baseUrl}/`),
+    randomStories(baseUrl, 5),
+    randomSections(baseUrl, 2)
+  ]);
+  const allPages = [homePage, ...stories, ...sections] as ReadonlyArray<
+    FetchUrlResponse
+  >;
 
-  const [robots, { seo, og, headers }, amp, { lighthouseSeo, lighthousePwa, pagespeed }, routeData] = await Promise.all([
+  const [
+    robots,
+    { seo, og, headers, assets },
+    amp,
+    { lighthouseSeo, lighthousePwa, pagespeed },
+    routeData
+  ] = await Promise.all([
     checkUrls(allPages.map(i => i.url)),
     checkHTTPResponses(allPages),
     checkAmp(stories),
@@ -172,8 +244,18 @@ export async function runAllChecksAgainstDomain(domain: string) {
 
   return {
     url: domain,
-    results: { robots, seo, og, headers, amp, pagespeed, lighthouseSeo, lighthousePwa, routeData },
+    results: {
+      robots,
+      seo,
+      og,
+      headers,
+      amp,
+      pagespeed,
+      lighthouseSeo,
+      lighthousePwa,
+      routeData,
+      assets
+    },
     links: allPages.map(p => p.url)
   };
 }
-
